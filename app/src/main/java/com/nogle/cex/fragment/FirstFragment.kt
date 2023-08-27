4b95ed65bab62f7107d7e347f1f085eeef94eb53
ricky7777 <risk0917@gmail.com>
2023-08-27 14:34:17 +0800

    [UI] add second fragment on exchange fragment

--- app/src/main/java/com/nogle/cex/fragment/FirstFragment.kt
+++ app/src/main/java/com/nogle/cex/fragment/FirstFragment.kt
@@ -5,18 +5,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.tabs.TabLayout
 import com.nogle.cex.R
+import com.nogle.cex.databinding.FragmentExchangeBinding
 
 /**
  * Created by Ricky on 2023/8/27.
  */
 class FirstFragment : Fragment() {
+    private var binding: FragmentExchangeBinding? = null
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exchange, container, false)
+        binding = FragmentExchangeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setNavigation()
+        setEvent()
+    }
+
+    private fun setNavigation() {
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.tab_container) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        navController.navigate(R.id.spotFragment)
+    }
+
+    private fun setEvent() {
+        binding?.tabLayout?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> navController.navigate(R.id.spotFragment)
+                    1 -> navController.navigate(R.id.futuresFragment)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
